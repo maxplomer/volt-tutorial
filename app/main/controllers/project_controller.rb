b8ad8f325495977cfebb18434538281a4b249997
Max Plomer <maxplomer@gmail.com>
2015-11-23 00:43:07 -0500

    project show page

--- app/main/controllers/project_controller.rb
+++ app/main/controllers/project_controller.rb
@@ -1,11 +1,20 @@
 module Main
-  class MainController < Volt::ModelController
+  class ProjectController < Volt::ModelController
     before_action :require_login
     
     def show
-
+      page._new_task_name = ''
     end
 
+    def add_new_task
+      unless page._new_task_name == ''
+        store._tasks << {
+          project_id: params.id,
+          name: page._new_task_name
+        }
+        page._new_task_name = ''
+      end
+    end
 
   end
 end
